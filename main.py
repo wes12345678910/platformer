4c796217ad80b1ef57c06656309040af90367f99
wes12345678910 <158511743+wes12345678910@users.noreply.github.com>
2025-02-12 20:45:24 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,30 @@
+def on_up_pressed():
+    hops_and_flops.vy = -200
+controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
+
+hops_and_flops: Sprite = None
+scene.set_background_color(9)
+hops_and_flops = sprites.create(img("""
+        . . . . . . . . . . b 5 b . . . 
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . . . . b b 5 d 1 f 5 d 4 c . . 
+            . . . . b 5 5 1 f f d d 4 4 4 b 
+            . . . . b 5 5 d f b 4 4 4 4 b . 
+            . . . b d 5 5 5 5 4 4 4 4 b . . 
+            . . b d d 5 5 5 5 5 5 5 5 b . . 
+            . b d d d d 5 5 5 5 5 5 5 5 b . 
+            b d d d b b b 5 5 5 5 5 5 5 b . 
+            c d d b 5 5 d c 5 5 5 5 5 5 b . 
+            c b b d 5 d c d 5 5 5 5 5 5 b . 
+            . b 5 5 b c d d 5 5 5 5 5 d b . 
+            b b c c c d d d d 5 5 5 b b . . 
+            . . . c c c c c c c c b b . . .
+    """),
+    SpriteKind.player)
+controller.move_sprite(hops_and_flops, 100, 0)
+tiles.set_current_tilemap(tilemap("""
+    level2
+"""))
+hops_and_flops.ay = 200
---
+++ main.ts
@@ -0,0 +1,27 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    hops_and_flops.vy = -150
+})
+let hops_and_flops: Sprite = null
+scene.setBackgroundColor(9)
+hops_and_flops = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 d 4 c . . 
+    . . . . b 5 5 1 f f d d 4 4 4 b 
+    . . . . b 5 5 d f b 4 4 4 4 b . 
+    . . . b d 5 5 5 5 4 4 4 4 b . . 
+    . . b d d 5 5 5 5 5 5 5 5 b . . 
+    . b d d d d 5 5 5 5 5 5 5 5 b . 
+    b d d d b b b 5 5 5 5 5 5 5 b . 
+    c d d b 5 5 d c 5 5 5 5 5 5 b . 
+    c b b d 5 d c d 5 5 5 5 5 5 b . 
+    . b 5 5 b c d d 5 5 5 5 5 d b . 
+    b b c c c d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(hops_and_flops, 100, 0)
+tiles.setCurrentTilemap(tilemap`level2`)
+hops_and_flops.ay = 200
+scene.cameraFollowSprite(hops_and_flops)
